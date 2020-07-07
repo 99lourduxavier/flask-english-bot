605f1313ef0073031770ef335e551916302fc18e
99lourduxavier <45066102+99lourduxavier@users.noreply.github.com>
2020-07-07 19:00:57 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,41 @@
+from flask import Flask,render_template,request,jsonify
+import requests
+import json
+
+#Flask object
+
+app=Flask(__name__,
+template_folder="client/template",
+static_folder="client/static")
+
+#load json file here
+with open("sentence.json", "r") as read_file:
+    word_in_sentence= json.load(read_file)
+
+#function to print sentence
+def print_sentence(word):
+    for item in word_in_sentence:
+        if word in item:
+             return item  
+        else:
+            msg="word is not available"
+            return msg       
+
+
+#root page 
+@app.route("/")
+def home_html():
+    print("html page printed sucessfully..!")
+    return render_template("english_bot.html")
+
+#function for api
+@app.route("/api/get_sentence",methods =['GET'])
+def word_present_sentence():
+    word_sentence = request.args.get("word")
+    print("got sentence for corresponding given word")
+    word_for_sentence = print_sentence(word_sentence)
+    return jsonify(word_for_sentence)
+
+
+if __name__ == '__main__':
+    app.run(debug=True,port=3000)
